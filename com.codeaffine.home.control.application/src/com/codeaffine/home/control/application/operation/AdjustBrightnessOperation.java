f836cee5789c505e3fffb6aaa2adcf03e8336061
FrankAppel <FrankAppel@192.168.0.23>
2017-12-30 15:33:27 +0100

    Introduction of manuell brightness and color temperature settings

--- com.codeaffine.home.control.application/src/com/codeaffine/home/control/application/operation/AdjustBrightnessOperation.java
+++ com.codeaffine.home.control.application/src/com/codeaffine/home/control/application/operation/AdjustBrightnessOperation.java
@@ -1,26 +1,35 @@
 package com.codeaffine.home.control.application.operation;
 
 import static com.codeaffine.home.control.status.type.Percent.*;
+import static com.codeaffine.home.control.type.OnOffType.ON;
 import static java.lang.Math.*;
 import static java.time.LocalDateTime.now;
 import static java.util.Arrays.asList;
 
 import java.util.Collection;
 
+import com.codeaffine.home.control.ByName;
 import com.codeaffine.home.control.application.lamp.LampProvider.Lamp;
 import com.codeaffine.home.control.application.lamp.LampProvider.LampDefinition;
 import com.codeaffine.home.control.entity.EntityProvider.EntityRegistry;
+import com.codeaffine.home.control.item.NumberItem;
+import com.codeaffine.home.control.item.SwitchItem;
 import com.codeaffine.home.control.status.HomeControlOperation;
 import com.codeaffine.home.control.status.StatusEvent;
 import com.codeaffine.home.control.status.StatusSupplier;
 import com.codeaffine.home.control.status.supplier.ActivitySupplier;
+import com.codeaffine.home.control.status.supplier.HeartBeatSupplier;
 import com.codeaffine.home.control.status.supplier.SunPositionSupplier;
 import com.codeaffine.home.control.status.type.Percent;
 
 
 public class AdjustBrightnessOperation implements HomeControlOperation {
 
+  private static final int BRIGHTNESS_DEFAULT = 50;
+
   private final EntityRegistry entityRegistry;
+  private final SwitchItem autoBrightness;
+  private final NumberItem brightnessItem;
 
   private Percent brightnessMiniumumBelowThreshold;
   private Percent brightnessMinimumAboveThreshold;
@@ -28,8 +37,13 @@ public class AdjustBrightnessOperation implements HomeControlOperation {
   private Percent activityThreshold;
   private Percent brightness;
 
-  public AdjustBrightnessOperation( EntityRegistry entityRegistry ) {
+  public AdjustBrightnessOperation( EntityRegistry entityRegistry,
+                                    @ByName( "autoBrightness" ) SwitchItem autoBrightnessSwitch,
+                                    @ByName( "brightness" ) NumberItem brightnessItem )
+  {
     this.entityRegistry = entityRegistry;
+    this.autoBrightness = autoBrightnessSwitch;
+    this.brightnessItem = brightnessItem;
     this.brightnessMinimum = P_001;
     this.brightness = P_100;
     reset();
@@ -49,7 +63,7 @@ public class AdjustBrightnessOperation implements HomeControlOperation {
 
   @Override
   public Collection<Class<? extends StatusSupplier<?>>> getRelatedStatusSupplierTypes() {
-    return asList( ActivitySupplier.class, SunPositionSupplier.class );
+    return asList( ActivitySupplier.class, SunPositionSupplier.class, HeartBeatSupplier.class );
   }
 
   @Override
@@ -66,18 +80,38 @@ public class AdjustBrightnessOperation implements HomeControlOperation {
       if( activity.getStatus().getOverallActivity().compareTo( activityThreshold ) > 1 ) {
         brightnessMinimum = brightnessMinimumAboveThreshold;
       }
-      adjustBrightnessOfLamps();
     } );
 
     event.getSource( SunPositionSupplier.class ).ifPresent( sunPositionSupplier -> {
-      double zenitAngle = sunPositionSupplier.getStatus().getZenit();
-      double brightnessFactor = min( 3.33, max( 2.0, ( abs( ( 10 + now().getDayOfYear() ) % 366 - 183 ) / 51.85 ) ) );
-      brightness = Percent.valueOf( ( int )max( brightnessMinimum.intValue(), min( ( ( zenitAngle + 18 ) * brightnessFactor ), 99.0 ) ) );
-      adjustBrightnessOfLamps();
+      if( isAutoBrightnessOn()) {
+        brightness = calculateBrightness( sunPositionSupplier );
+        adjustBrightnessOfLamps( brightness );
+      } else {
+        adjustBrightnessOfLamps( getBrightnessSetting() );
+      }
     } );
+
+    event.getSource( HeartBeatSupplier.class ).ifPresent( heartBeat -> {
+      adjustBrightnessOfLamps( isAutoBrightnessOn() ? brightness : getBrightnessSetting() );
+    });
   }
 
-  private void adjustBrightnessOfLamps() {
+  private Percent getBrightnessSetting() {
+    return Percent.valueOf( brightnessItem.getStatus( BRIGHTNESS_DEFAULT ) );
+  }
+
+  private boolean isAutoBrightnessOn() {
+    return autoBrightness.getStatus( ON ) == ON;
+  }
+
+  private Percent calculateBrightness( SunPositionSupplier sunPositionSupplier ) {
+    double zenitAngle = sunPositionSupplier.getStatus().getZenit();
+    double brightnessFactor = min( 3.33, max( 2.0, ( abs( ( 10 + now().getDayOfYear() ) % 366 - 183 ) / 51.85 ) ) );
+    return Percent.valueOf( ( int )max( brightnessMinimum.intValue(),
+                                        min( ( ( zenitAngle + 18 ) * brightnessFactor ), 99.0 ) ) );
+  }
+
+  private void adjustBrightnessOfLamps( Percent brightness ) {
     Collection<Lamp> lamps = entityRegistry.findByDefinitionType( LampDefinition.class );
     lamps.forEach( lamp -> lamp.setBrightness( brightness ) );
   }
--- com.codeaffine.home.control.application/src/com/codeaffine/home/control/application/operation/AdjustColorTemperatureOperation.java
+++ com.codeaffine.home.control.application/src/com/codeaffine/home/control/application/operation/AdjustColorTemperatureOperation.java
@@ -1,31 +1,47 @@
 package com.codeaffine.home.control.application.operation;
 
+import static com.codeaffine.home.control.type.OnOffType.ON;
 import static java.lang.Math.*;
 import static java.time.LocalDateTime.now;
 import static java.util.Arrays.asList;
 
 import java.util.Collection;
 
+import com.codeaffine.home.control.ByName;
 import com.codeaffine.home.control.application.lamp.LampProvider.Lamp;
 import com.codeaffine.home.control.application.lamp.LampProvider.LampDefinition;
-import com.codeaffine.home.control.status.supplier.SunPositionSupplier;
-import com.codeaffine.home.control.status.type.Percent;
 import com.codeaffine.home.control.entity.EntityProvider.EntityRegistry;
+import com.codeaffine.home.control.item.NumberItem;
+import com.codeaffine.home.control.item.SwitchItem;
 import com.codeaffine.home.control.status.HomeControlOperation;
 import com.codeaffine.home.control.status.StatusEvent;
 import com.codeaffine.home.control.status.StatusSupplier;
+import com.codeaffine.home.control.status.supplier.HeartBeatSupplier;
+import com.codeaffine.home.control.status.supplier.SunPositionSupplier;
+import com.codeaffine.home.control.status.type.Percent;
 
 public class AdjustColorTemperatureOperation implements HomeControlOperation {
 
+  private static final int COLOR_TEMPERATURE_DEFAULT = 50;
+
+  private final SwitchItem autoColorTemperatureSwitch;
+  private final NumberItem colorTemperatureItem;
   private final EntityRegistry entityRegistry;
 
-  public AdjustColorTemperatureOperation( EntityRegistry entityRegistry ) {
+  private Percent colorTemperature;
+
+  public AdjustColorTemperatureOperation( EntityRegistry entityRegistry,
+                                          @ByName( "autoColorTemperature" ) SwitchItem autoColorTemperatureSwitch,
+                                          @ByName( "colorTemperature" ) NumberItem colorTemperatureItem ) {
+    this.autoColorTemperatureSwitch = autoColorTemperatureSwitch;
+    this.colorTemperatureItem = colorTemperatureItem;
     this.entityRegistry = entityRegistry;
+    this.colorTemperature = getColorTemperatureSetting();
   }
 
   @Override
   public Collection<Class<? extends StatusSupplier<?>>> getRelatedStatusSupplierTypes() {
-    return asList( SunPositionSupplier.class );
+    return asList( SunPositionSupplier.class, HeartBeatSupplier.class );
   }
 
   @Override
@@ -35,12 +51,36 @@ public class AdjustColorTemperatureOperation implements HomeControlOperation {
   @Override
   public void executeOn( StatusEvent event ) {
     event.getSource( SunPositionSupplier.class ).ifPresent( sunPositionSupplier -> {
-      double zenitAngle = sunPositionSupplier.getStatus().getZenit();
-      double temperatureFactor = max( 2.0, ( abs( ( 10 + now().getDayOfYear() ) % 366 - 183 ) / 31 ) );
-      double value = min( ( ( zenitAngle + 7 ) * temperatureFactor ), 100.0 );
-      Percent colorTemperature = Percent.valueOf( ( int )( 100.0 - max( 0.0, value ) ) );
-      Collection<Lamp> lamps = entityRegistry.findByDefinitionType( LampDefinition.class );
-      lamps.forEach( lamp -> lamp.setColorTemperature( colorTemperature ) );
+      if( isAutoColorTemperatureOn() ) {
+        colorTemperature = calculateColorTemperature( sunPositionSupplier );
+        adjustColorTemperatureOfLamps( colorTemperature );
+      } else {
+        adjustColorTemperatureOfLamps( getColorTemperatureSetting() );
+      }
     } );
+
+    event.getSource( HeartBeatSupplier.class ).ifPresent( heartBeat -> {
+      adjustColorTemperatureOfLamps( isAutoColorTemperatureOn() ? colorTemperature : getColorTemperatureSetting() );
+    });
+  }
+
+  private boolean isAutoColorTemperatureOn() {
+    return autoColorTemperatureSwitch.getStatus( ON ) == ON;
+  }
+
+  private static Percent calculateColorTemperature( SunPositionSupplier sunPositionSupplier ) {
+    double zenitAngle = sunPositionSupplier.getStatus().getZenit();
+    double temperatureFactor = max( 2.0, ( abs( ( 10 + now().getDayOfYear() ) % 366 - 183 ) / 31 ) );
+    double value = min( ( ( zenitAngle + 7 ) * temperatureFactor ), 100.0 );
+    return Percent.valueOf( ( int )( 100.0 - max( 0.0, value ) ) );
+  }
+
+  private void adjustColorTemperatureOfLamps( Percent colorTemperature ) {
+    Collection<Lamp> lamps = entityRegistry.findByDefinitionType( LampDefinition.class );
+    lamps.forEach( lamp -> lamp.setColorTemperature( colorTemperature ) );
+  }
+
+  private Percent getColorTemperatureSetting() {
+    return Percent.valueOf( colorTemperatureItem.getStatus( COLOR_TEMPERATURE_DEFAULT ) );
   }
 }
