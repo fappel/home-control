216e4b6f95d2fde9a2a9f24ee4b485bb482018e1
FrankAppel <FrankAppel@192.168.0.23>
2018-02-13 17:54:02 +0100

    Introduction of various Lamp Brightness Scene Preferences

--- com.codeaffine.home.control.application/src/com/codeaffine/home/control/application/scene/BathroomScene.java
+++ com.codeaffine.home.control.application/src/com/codeaffine/home/control/application/scene/BathroomScene.java
@@ -5,16 +5,23 @@ import static com.codeaffine.home.control.application.scene.HomeScope.BATH_ROOM;
 
 import java.util.Optional;
 
+import com.codeaffine.home.control.application.operation.AdjustBrightnessOperation;
 import com.codeaffine.home.control.application.util.LampControl;
 import com.codeaffine.home.control.status.Scene;
 import com.codeaffine.home.control.status.SceneSelector.Scope;
 
 public class BathroomScene implements Scene {
 
+  private final AdjustBrightnessOperation brightnessOperation;
+  private final BathroomScenePreference preference;
   private final LampControl lampControl;
 
-  public BathroomScene( LampControl lampControl ) {
+  public BathroomScene(
+    LampControl lampControl, AdjustBrightnessOperation brightnessOperation, BathroomScenePreference preference )
+  {
+    this.brightnessOperation = brightnessOperation;
     this.lampControl = lampControl;
+    this.preference = preference;
   }
 
   @Override
@@ -30,5 +37,6 @@ public class BathroomScene implements Scene {
   @Override
   public void prepare() {
     lampControl.switchOnLamps( BathRoomCeiling );
+    brightnessOperation.adjustLampMiniumBrightness( BathRoomCeiling, preference.getLampMinimumBrightness() );
   }
 }
